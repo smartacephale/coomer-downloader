0b32b9ad41ee8af43af6a410a70837d1c010259e
smartacephale <atm.mormon@protonmail.com>
2025-01-13 06:31:08 +0200

    do not redownload existing files

--- index.js
+++ index.js
@@ -7,130 +7,131 @@ import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
 const argv = yargs(hideBin(process.argv))
-    .option("url", {
-        alias: "u",
-        type: "string",
-        description: "Coomer/Kemono URL",
-        demandOption: true,
-    })
-    .option("dir", {
-        type: "string",
-        description: "Directory to download files to",
-        default: "./",
-    })
-    .option("media", {
-        type: "string",
-        choices: ["video", "image", "all"],
-        default: "all",
-        description:
-            "Download media type: 'video', 'image', or 'all', 'all' is default",
-    })
-    .help()
-    .alias("help", "h").argv;
+	.option("url", {
+		alias: "u",
+		type: "string",
+		description: "Coomer/Kemono URL",
+		demandOption: true,
+	})
+	.option("dir", {
+		type: "string",
+		description: "Directory to download files to",
+		default: "./",
+	})
+	.option("media", {
+		type: "string",
+		choices: ["video", "image", "all"],
+		default: "all",
+		description:
+			"Download media type: 'video', 'image', or 'all', 'all' is default",
+	})
+	.help()
+	.alias("help", "h").argv;
 
 const isImage = (name) =>
-    /\.(jpg|jpeg|png|gif|bmp|tiff|webp|avif|svg|ico)$/i.test(name);
+	/\.(jpg|jpeg|png|gif|bmp|tiff|webp|avif|svg|ico)$/i.test(name);
 
 const isVideo = (name) =>
-    /\.(mp4|avi|mov|mkv|webm|flv|wmv|mpeg|mpg|3gp)$/i.test(name);
+	/\.(mp4|avi|mov|mkv|webm|flv|wmv|mpeg|mpg|3gp)$/i.test(name);
 
 const testMediaType = (name, type) =>
-    type === "all" ? true : type === "image" ? !isVideo(name) : !isImage(name);
+	type === "all" ? true : type === "image" ? !isVideo(name) : !isImage(name);
 
 async function downloadFiles(data, downloadDir) {
-    if (!fs.existsSync(downloadDir)) {
-        fs.mkdirSync(downloadDir, { recursive: true });
-    }
+	if (!fs.existsSync(downloadDir)) {
+		fs.mkdirSync(downloadDir, { recursive: true });
+	}
 
-    for (const [index, { name, src }] of data.entries()) {
-        try {
-            const response = await fetch(src);
-            if (!response.ok) {
-                console.error(`\nFailed to download ${src}: ${response.statusText}`);
-                continue;
-            }
+	for (const [index, { name, src }] of data.entries()) {
+		try {
+			const response = await fetch(src);
+			if (!response.ok) {
+				console.error(`\nFailed to download ${src}: ${response.statusText}`);
+				continue;
+			}
 
-            const filePath = path.join(downloadDir, name);
-            const fileStream = fs.createWriteStream(filePath);
+			const filePath = path.join(downloadDir, name);
+			if (fs.existsSync(filePath)) continue;
+			const fileStream = fs.createWriteStream(filePath);
 
-            await new Promise((resolve, reject) => {
-                response.body.pipe(fileStream);
-                response.body.on("error", reject);
-                fileStream.on("finish", resolve);
-            });
+			await new Promise((resolve, reject) => {
+				response.body.pipe(fileStream);
+				response.body.on("error", reject);
+				fileStream.on("finish", resolve);
+			});
 
-            process.stdout.write(`\rDownloading files: ${index + 1}/${data.length}`);
-        } catch (error) {
-            console.error(`\nError downloading ${name}:`, error.message);
-        }
-    }
-    console.log("\n");
+			process.stdout.write(`\rDownloading files: ${index + 1}/${data.length}`);
+		} catch (error) {
+			console.error(`\nError downloading ${name}:`, error.message);
+		}
+	}
+	console.log("\n");
 }
 
 const userProfileAPI = ({ domain, service, id }) =>
-    `${domain}/api/v1/${service}/user/${id}/profile`;
+	`${domain}/api/v1/${service}/user/${id}/profile`;
 const userPostsAPI = (user, offset) =>
-    `${user.domain}/api/v1/${user.service}/user/${user.id}?o=${offset}`;
+	`${user.domain}/api/v1/${user.service}/user/${user.id}?o=${offset}`;
 
 async function getUserFiles(user, typeFilter) {
-    const userPosts = [];
+	const userPosts = [];
 
-    const offset = 50;
-    for (let i = 0; i < 1000; i++) {
-        const posts = await fetch(userPostsAPI(user, offset * i)).then((r) =>
-            r.json(),
-        );
-        userPosts.push(...posts);
-        if (posts.length < 50) break;
-    }
+	const offset = 50;
+	for (let i = 0; i < 1000; i++) {
+		const posts = await fetch(userPostsAPI(user, offset * i)).then((r) =>
+			r.json(),
+		);
+		userPosts.push(...posts);
+		if (posts.length < 50) break;
+	}
 
-    const files = [];
+	const files = [];
 
-    for (const p of userPosts) {
-        const title = p.title.match(/\w+/g)?.join(" ") || "";
-        const date = p.published.replace(/T/, " ");
-        const datentitle = `${date} ${title}`.trim();
+	for (const p of userPosts) {
+		const title = p.title.match(/\w+/g)?.join(" ") || "";
+		const date = p.published.replace(/T/, " ");
+		const datentitle = `${date} ${title}`.trim();
 
-        const postFiles = [...p.attachments, p.file]
-            .filter((f) => f.path)
-            .filter((f) => testMediaType(f.name, typeFilter))
-            .map((f, i) => {
-                const ext = f.name.split(".").pop();
-                const name = `${datentitle} ${i}.${ext}`;
-                const src = `${user.domain}/${f.path}`;
-                return { name, src };
-            });
+		const postFiles = [...p.attachments, p.file]
+			.filter((f) => f.path)
+			.filter((f) => testMediaType(f.name, typeFilter))
+			.map((f, i) => {
+				const ext = f.name.split(".").pop();
+				const name = `${datentitle} ${i + 1}.${ext}`;
+				const src = `${user.domain}/${f.path}`;
+				return { name, src };
+			});
 
-        files.push(...postFiles);
-    }
+		files.push(...postFiles);
+	}
 
-    return files;
+	return files;
 }
 
 async function parseUser(url) {
-    const [_, domain, service, id] = url.match(
-        /(https:\/\/\w+\.\w+)\/(\w+)\/user\/([\w|\.|-]+)/,
-    );
-    if (!domain || !service || !id) console.error("Invalid URL", url);
+	const [_, domain, service, id] = url.match(
+		/(https:\/\/\w+\.\w+)\/(\w+)\/user\/([\w|\.|-]+)/,
+	);
+	if (!domain || !service || !id) console.error("Invalid URL", url);
 
-    const { name } = await fetch(userProfileAPI({ domain, service, id })).then(
-        (r) => r.json(),
-    );
+	const { name } = await fetch(userProfileAPI({ domain, service, id })).then(
+		(r) => r.json(),
+	);
 
-    return { domain, service, id, name };
+	return { domain, service, id, name };
 }
 
 async function run() {
-    const { url, dir, media } = argv;
-    const user = await parseUser(url);
-    const userDir = `${user.name}-${user.service}`;
-    const downloadDir =
-        dir === "./"
-            ? path.resolve(dir, userDir)
-            : path.join(os.homedir(), path.join(dir, userDir));
-    const files = await getUserFiles(user, media);
-    console.log(files.length, "files found");
-    await downloadFiles(files, downloadDir);
+	const { url, dir, media } = argv;
+	const user = await parseUser(url);
+	const userDir = `${user.name}-${user.service}`;
+	const downloadDir =
+		dir === "./"
+			? path.resolve(dir, userDir)
+			: path.join(os.homedir(), path.join(dir, userDir));
+	const files = await getUserFiles(user, media);
+	console.log(files.length, "files found");
+	await downloadFiles(files, downloadDir);
 }
 
 run();
