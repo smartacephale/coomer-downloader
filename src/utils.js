3dce92795e92dac1f063400b4f43d767f63944ba
smartacephale <atm.mormon@protonmail.com>
2025-01-14 05:09:20 +0200

    media-type

--- src/utils.js
+++ src/utils.js
@@ -5,4 +5,4 @@ export const isVideo = (name) =>
   /\.(mp4|m4v|avi|mov|mkv|webm|flv|wmv|mpeg|mpg|3gp)$/i.test(name);
 
 export const testMediaType = (name, type) =>
-  type === 'all' ? true : type === 'image' ? !isVideo(name) : !isImage(name);
+  type === 'all' ? true : type === 'image' ? isImage(name) : isVideo(name);
